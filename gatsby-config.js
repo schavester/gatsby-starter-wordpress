8a556b90cb3c3a51a2f73ce660fa7fb3b2403bcc
Nicholas <nicholashayes@mail.weber.edu>
2019-03-24 20:13:58 -0600

    Made the header for the site

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,29 +1,24 @@
 module.exports = {
-  siteMetadata: {
-    title: 'Gatsby + WordPress Starter',
-  },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass',
-    {
-      resolve: 'gatsby-source-wordpress',
-      options: {
-        // The base url to your WP site.
-        baseUrl: 'insroland.hiddenhistoryblogs.com',
-        // WP.com sites set to true, WP.org set to false
-        hostingWPCOM: false,
-        // The protocol. This can be http or https.
-        protocol: 'https',
-        // Use 'Advanced Custom Fields' Wordpress plugin
-        useACF: false,
-        auth: {},
-        // Set to true to debug endpoints on 'gatsby build'
-        verboseOutput: true,
-      },
+    siteMetadata: {
+        title: 'Title from siteMetaData',
+        subtitle: 'Subtitle  from siteMetaData'
     },
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-purgecss',
-    'gatsby-plugin-netlify', // make sure to keep it last in the array
-  ],
+    plugins: [
+        'gatsby-plugin-react-helmet',
+        'gatsby-plugin-sass',
+        {
+            resolve: 'gatsby-source-wordpress',
+            options: {
+                baseUrl: 'localhost/wp',
+                hostingWPCOM: false,
+                protocol: 'http',
+                useACF: false,
+                verboseOutput: true,
+            },
+        },
+        'gatsby-plugin-sharp',
+        'gatsby-transformer-sharp',
+        'gatsby-plugin-purgecss',
+        'gatsby-plugin-netlify', // make sure to keep it last in the array
+    ]
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import { StaticQuery, graphql, Link } from 'gatsby'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircle } from '@fortawesome/free-solid-svg-icons'
+import { faFacebookF, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons'
+import styles from "./Navbar.module.css"
+
+library.add(faFacebookF, faTwitter, faYoutube, faCircle)
+
+const NavBar = () => (
+  <StaticQuery
+    query={graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            subtitle
+          }
+        }
+      }
+    `}
+    render={data => (
+        <div className={styles.header}>
+            <div className={styles.header_img}>
+              <div className={styles.title}>
+                  {data.site.siteMetadata.title}
+              </div>
+              <div className={styles.subtitle}>
+                  {data.site.siteMetadata.subtitle}
+              </div>
+              <div className={styles.socialMediaButtonRow}>
+                <a className={styles.iconButton} href="https://www.facebook.com/1947project#" target="_blank" rel="noopener noreferrer">
+                  <span className="fa-layers">
+                    <FontAwesomeIcon className={`${styles.circleIcon}`} icon={["fas", "circle"]} size="2x"/>
+                    <FontAwesomeIcon className={`${styles.icon}`} icon={["fab", "facebook-f"]} transform="right-5"/>
+                  </span>
+                </a>
+                <a className={styles.iconButton} href="https://twitter.com/1947project" target="_blank" rel="noopener noreferrer">
+                  <span className="fa-layers">
+                    <FontAwesomeIcon className={`${styles.circleIcon}`} icon={["fas", "circle"]} size="2x"/>
+                    <FontAwesomeIcon className={styles.icon} icon={["fab", "twitter"]} transform="right-3"/>
+                  </span>
+                </a>
+                <a className={styles.iconButton} href="https://www.youtube.com/user/esotouric" target="_blank" rel="noopener noreferrer">
+                  <span className="fa-layers">
+                    <FontAwesomeIcon className={`${styles.circleIcon}`} icon={["fas", "circle"]} size="2x"/>
+                    <FontAwesomeIcon className={styles.icon} icon={["fab", "youtube"]} transform="right-2"/>
+                  </span>
+                </a>
+              </div>
+              <div className={styles.navBar}>
+                <Link className={styles.navBarOption} to="/about">About</Link>
+                <Link className={styles.navBarOption} to="/authors">Authors</Link>
+                <Link className={styles.navBarOption} to="/timetravelblogs">Our Time Travel Blogs</Link>
+              </div>
+            </div>
+        </div>
+    )}
+  />
+)
+
+export default NavBar
